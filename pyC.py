061e573647b2f36a64d81dbf2acb134c4bc8eb8e
David Karesh <davidnk@gmail.com>
2014-02-13 15:14:28 -0500

    Switch to using current directory

--- pyC.py
+++ pyC.py
@@ -6,13 +6,13 @@ def runC(functions='', run='', imports=[]):
     prog = []
     for i in imports:
         prog.append("#include {}".format(i))
-    prog.append(function)
+    prog.append(functions)
     prog.append("int main() {")
     prog.append(run + ';')
     prog.append("return 0;}")
-    with open("/tmp/tempC.c", "w") as c:
+    with open("__pyC_tempC.c", "w") as c:
         c.write('\n'.join(prog))
-    os.system("gcc /tmp/tempC.c -o /tmp/tempC.x")
-    ret = subprocess.check_output("/tmp/tempC.x")
-    os.system("rm /tmp/tempC.x /tmp/tempC.c")
+    os.system("gcc __pyC_tempC.c -o __pyC_tempC.x")
+    ret = subprocess.check_output("./__pyC_tempC.x")
+    os.system("rm __pyC_tempC.x __pyC_tempC.c")
     return ret
